272ff0bb638f510f93eef166dce80b3d845b4acf
liulihui3 <liulihui3@360buyad.local>
2018-05-16 17:49:20 +0800

    今天过去了

--- progress/pages/shopAssistant/realTimePerformance/guestGroup/guestGroup.js
+++ progress/pages/shopAssistant/realTimePerformance/guestGroup/guestGroup.js
@@ -9,6 +9,24 @@ Page({
     currentTab: 1 ,
     dateOpt: ['日','周','月'],
     currentDate: 0, 
+    // 下拉框
+    show: false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    selectData: ['按照关注人数排序', '按照关注总时长排序'],//下拉列表的数据
+    index: 0//选择的下拉列表下标
+  },
+  // 点击下拉显示框
+  selectTap() {
+    this.setData({
+      show: !this.data.show
+    });
+  },
+  // 点击下拉列表
+  optionTap(e) {
+    let Index = e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+    this.setData({
+      index: Index,
+      show: !this.data.show
+    });
   },
   navbarTap: function (e) {
     this.setData({
